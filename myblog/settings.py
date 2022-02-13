d8e91ced89319adaf45a45db752029309de93b9a
wlan07 <albertoalberto58940053@gmail.com>
2022-02-13 03:54:37 +0100

    add a rich text editor to blog's body field ✨

--- myblog/settings.py
+++ myblog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myblogapp',
-    'users'
+    'users',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
--- myblogapp/models.py
+++ myblogapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
@@ -18,14 +18,14 @@ def get_default_category() -> int:
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = RichTextField(blank=True, null=True)
+    #body = models.TextField()
     category = models.ForeignKey(
         Category, default=get_default_category, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User,related_name="blog_likes")
-
+    likes = models.ManyToManyField(User, related_name="blog_likes")
 
     def count_likes(self):
         return self.likes.count()
-        
+
     def __str__(self) -> str:
         return self.title + " | " + str(self.author)
