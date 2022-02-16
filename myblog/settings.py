736db5144f4e8d7c60bbc76be473a5600377c9eb
wlan07 <albertoalberto58940053@gmail.com>
2022-02-16 23:53:31 +0100

    implement default profile picture (static files) ✨

--- myblog/settings.py
+++ myblog/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+
+STATICFILES_DIRS = [
+ os.path.join(BASE_DIR, "static")
+]
+   
+
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 """STATICFILES_DIRS = (
