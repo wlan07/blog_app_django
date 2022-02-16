936c2f3b018cb5ca30c5bdc34ef2a756fdd40fe5
wlan07 <albertoalberto58940053@gmail.com>
2022-02-16 23:25:14 +0100

    customize user profile model & add author info card in the blog-detail page ✨🔥

--- myblogapp/models.py
+++ myblogapp/models.py
@@ -15,14 +15,16 @@ def get_default_category() -> int:
     return Category.objects.get(name="Coding").id
 
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    header_image = models.ImageField(null=True,blank=True,upload_to="images/")
+    header_image = models.ImageField(
+        null=True, blank=True, upload_to="images/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
     category = models.ForeignKey(
         Category, default=get_default_category, on_delete=models.CASCADE)
-    snippet = models.CharField(max_length=200,default="NO SNIPPET HERE!")
+    snippet = models.CharField(max_length=200, default="NO SNIPPET HERE!")
     likes = models.ManyToManyField(User, related_name="blog_likes")
 
     def count_likes(self):
@@ -30,3 +32,20 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title + " | " + str(self.author)
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,  null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(
+        null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(null=True,blank=True,max_length=255)
+    fb_url = models.CharField(null=True,blank=True,max_length=255)
+    instagram_url = models.CharField(null=True,blank=True,max_length=255)
+    linkedIn_url = models.CharField(null=True,blank=True,max_length=255)
+    github_url = models.CharField(null=True,blank=True,max_length=255)
+
+    def __str__(self) -> str:
+        return str(self.user)
